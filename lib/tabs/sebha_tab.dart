492f288149b9792467dfec9189e5f7a8cea2a622
osama romeh <osamaromeh42@gmail.com>
2023-09-20 15:13:13 +0300

    add dark theme and enable this Finish Dark Mode Install Dark mode and made a switch between Themes on my app complete 90% of Themeing My Application complete 98% of Themeing My Application Complete sebha Task

--- lib/tabs/sebha_tab.dart
+++ lib/tabs/sebha_tab.dart
@@ -2,73 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:islami_app/my_Theme_Data.dart';
 
 class SebhaTab extends StatefulWidget {
+  const SebhaTab({super.key});
+
   @override
   State<SebhaTab> createState() => _SebhaTabState();
 }
 
 class _SebhaTabState extends State<SebhaTab> {
-  int num=0;
-String tasabeh=" ";
+  int num = 0;
+  int Counter=0;
+  String tasabeh = " سبحان الله";
+
   @override
   Widget build(BuildContext context) {
-    return
-      Center(
+    return Center(
       child: Column(
-        children:[
-          Image.asset("assets/images/head_sebha.png",color: MyThemeData.PrimaryColor),
-          SizedBox(height:30.0),
-          Text("عدد التسبيحات ",
-              style: Theme.of(context).textTheme.bodySmall!
-          ),
-          SizedBox(height: 20.0),
-
-
-        Container
-          (
-            height: 70,
-            width: 55,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0),
-              color: MyThemeData.PrimaryColor,
-            ),
-            child: Center(child: Text("$num",)
-
-        )
-        ),
-        SizedBox(height: 30.0),
-        Container(
+        children: [
+          Image.asset("assets/images/head_sebha.png",
+              color: MyThemeData.PrimaryColor),
+          const SizedBox(height: 30.0),
+          Text("عدد التسبيحات ", style: Theme.of(context).textTheme.bodySmall!),
+          const SizedBox(height: 20.0),
+          Container(
+              height: 70,
+              width: 55,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: MyThemeData.PrimaryColor,
+              ),
+              child: Center(
+                  child: Text(
+                "$num",
+              ))),
+          const SizedBox(height: 30.0),
+          SizedBox(
             height: 50,
             width: 160,
-          child: ElevatedButton(
-            onPressed: () {
-              num++;
-              setState(() {
-              });
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: MyThemeData.PrimaryColor,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)
+              ),
 
-            },
-            child:
-            Container(
-                height: 50,
-                width: 160,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0),
-                  color: MyThemeData.PrimaryColor,
-                ),
-                child: Center(child: Text("سبحان الله ",
-                  style: Theme.of(context).textTheme.bodySmall!,
-                )
-            )
+              
+              ),
+              onPressed: () {
+                {
+                  for(int i=num;i<132;i++)
+                  {
+                    if(num==33){
+                      tasabeh="الحمدلله";
+                    }else if(num==66)
+                      {
+                        tasabeh="لا اله الا الله";
+                      }else if(num==99)
+                        {
+                          tasabeh="الله اكبر";
+                        }
+                  }
+                  setState(() {
+
+                    num++;
+
+                  });
+
+                }
+
+
+              },
+              child: Center(
+                  child: Text(tasabeh,
+                style: Theme.of(context).textTheme.bodySmall!,
+              )),
             ),
           ),
-        ),
-
-
-
-
         ],
       ),
     );
   }
-
-
-
-  }
-
+}
